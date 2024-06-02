390991324db6d7f5ce8a4f1f8b258bfbf4bf04f2
Luiz Gustavo Da Silva Moraes <luizgm.dev@gmail.com>
2024-06-02 01:31:28 -0300

    update pokemon info

--- App/src/components/Info.jsx
+++ App/src/components/Info.jsx
@@ -7,6 +7,7 @@ const Info = ({ num }) => {
 
 
     const [pokemon, setPokemon] = useState(null)
+    const [Stage, setStage] = useState('status')
     const { FetchPokemon } = useFetchPokemons()
 
     useEffect(() => {
@@ -35,7 +36,72 @@ const Info = ({ num }) => {
                         ))}
                     </div>
 
-                    <div className='informacoes'></div>
+                    <div className='constainer'>
+                        <div className='abas'>
+                            <div className={`sobre ${Stage === 'sobre' ? 'activeAba' : ''}`} onClick={() => setStage('sobre')}>
+                                <h2>Sobre</h2>
+                            </div>
+                            <div className={`moves ${Stage === 'moves' ? 'activeAba' : ''}`} onClick={() => setStage('moves')}>
+                                <h2>Moves</h2>
+                            </div>
+                            <div className={`status ${Stage === 'status' ? 'activeAba' : ''}`} onClick={() => setStage('status')}>
+                                <h2>Status</h2>
+                            </div>
+                        </div>
+                        <div className='informacoes'>
+                            {Stage === 'status' &&
+                                <div className='stats'>
+                                    <h1>Status base</h1>
+                                    <span>
+                                        <p>{pokemon.stats[0].stat.name}:</p>
+                                        <p>{pokemon.stats[0].base_stat}</p>
+                                    </span>
+                                    <hr />
+                                    <span>
+                                        <p>{pokemon.stats[1].stat.name}:</p>
+                                        <p>{pokemon.stats[1].base_stat}</p>
+                                    </span>
+                                    <hr />
+                                    <span>
+                                        <p>{pokemon.stats[2].stat.name}:</p>
+                                        <p>{pokemon.stats[2].base_stat}</p>
+                                    </span>
+                                    <hr />
+                                    <span>
+                                        <p>{pokemon.stats[3].stat.name}:</p>
+                                        <p>{pokemon.stats[3].base_stat}</p>
+                                    </span>
+                                    <hr />
+                                    <span>
+                                        <p>{pokemon.stats[4].stat.name}:</p>
+                                        <p>{pokemon.stats[4].base_stat}</p>
+                                    </span>
+                                    <hr />
+                                    <span>
+                                        <p>{pokemon.stats[5].stat.name}:</p>
+                                        <p>{pokemon.stats[5].base_stat}</p>
+                                    </span>
+                                    <hr />
+                                </div>
+                            }
+                            {Stage === 'moves' &&
+                                <div></div>
+                            }
+                            {Stage === 'sobre' &&
+                                <>
+                                    <div>
+                                        <span>Height:{pokemon.height}</span>
+                                        <span>Weight:{pokemon.weight}</span>
+                                    </div>
+                                    <div>{pokemon.abilities.map((ability) => (
+                                        <p>{ability.ability.name}</p>
+                                    ))}</div>
+                                </>
+
+                            }
+
+                        </div>
+                    </div>
 
                 </>
             }
